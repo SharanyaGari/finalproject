478a4e5bb9c1aae8c61bd14e9dbbeb3e5c82b0ae
Sharanya Garipally <sharanya@Sharanyas-MacBook-Pro.local>
2024-04-22 16:33:21 -0400

    Changed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
           <Route path="/" element={<HomePage/>}/>
         </Routes>
        </div>
-       <Footer></Footer>
+       <Footer initialCount={0}></Footer>
 
 
     </Router>
--- frontend/src/Footer/Footer.js
+++ frontend/src/Footer/Footer.js
@@ -1,14 +1,38 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function Footer() {
+
+  const initialCount = 0
+  
+  const [count, setCount] = useState(initialCount)
+
+  const increaseReview = () => {
+    setCount((prev) => prev + 1)
+  }
+
+  const decreaseReview = () => {
+    setCount((prev) => prev - 1)
+  }
   return (
     <footer className="bottom">
         <div className="center">
-            All rights reserved &copy; Fabio Nolasco
+            All rights reserved &copy; Sharanya Garipally
+        </div>
+        <div>
+          <h1>
+            review: <h4 data-testid="count">{count}</h4>
+          </h1>
+            
+          
+        </div>
+        <div>
+          <button onClick={increaseReview}>Like</button>
+          <button onClick={decreaseReview}>disLike</button>
         </div>
     </footer>
   );
+  
 }
 
 export default Footer;
